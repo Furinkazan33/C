84bc552312378c84c43b0547c625f37f16c59e75
Mathieu Vidalies <mathieu.vidalies@gmail.com>
2023-06-22 15:29:08 +0200

    Suppression fichiers inutiles

--- lib/main_str.c
+++ lib/main_str.c
@@ -1,21 +1,41 @@
-#include "str.h"
+#include <string.h>
+#include <stdio.h>
 
+char *str_rpl(char *src, char *pattern, char *rpl) {
+  char *dest, *origin;
+  char *found = strstr(src, pattern);
+
+  if(!found) {
+    return NULL;
+  }
+
+  /* Shifting right or left to fit new size */
+  dest = found;
+  origin = found + strlen(pattern);
+
+  if(strlen(rpl) > strlen(pattern)) {
+    dest += strlen(rpl);
+  }
+  else {
+    origin -= strlen(rpl);
+  }
+  memmove(dest, origin, strlen(origin) + 1);
+
+  /* Replacement */
+  memcpy(found, rpl, strlen(rpl));
+
+  return found;
+}
 
 
 
 
 int main(void) {
-	char src[] = "1111pat";
-	int idx = str_idx(src, "pat");
-	char *found = str_find(src, "pat");
+	char src[100] = "un test de string";
+	char *replace = "remplacement de string";
+	str_rpl(src, "string", replace);
 
-	if(idx) {
-		printf("%s\n", src+idx);
-	}
-
-	if(found) {
-		printf("%s\n", found);
-	}
+	printf("%s\n", src);
 
 	return 0;
 }
